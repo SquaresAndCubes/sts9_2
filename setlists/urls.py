141705d5661e9bb58a29a9e2c566cf417afe296c
squaresandcubes <btvaalburg@gmail.com>
2018-05-16 02:09:41 -0600

    +Site struct and Temp method calls

--- setlists/urls.py
+++ setlists/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     #About section
     path('test/', views.test, name='test'),
+    path('', views.setlists, name='setlists'),
+    path('', views.songs, name='songs'),
 
 ]
--- setlists/views.py
+++ setlists/views.py
@@ -14,3 +14,26 @@ def test(request):
         'setlists': setlists,
     }
     return HttpResponse(template.render(context, request))
+
+def setlists(request):
+
+    setlists = Show.objects.all()
+
+    template = loader.get_template('setlists/index.html')
+
+    context = {
+        'setlists': setlists,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def songs(request):
+
+    songs = Song.objects.all()
+
+    template = loader.get_template('songs/index.html')
+
+    context = {
+        'songs': songs,
+    }
+    return HttpResponse(template.render(context, request))
